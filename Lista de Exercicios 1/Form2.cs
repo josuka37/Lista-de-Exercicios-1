283dd940f2380b13408f44ce820e45e0effafc15
Josué da Motta <josuka37@gmail.com>
2021-06-25 19:27:38 -0300

    Adicionar arquivos de projeto.

---
+++ Lista de Exercicios 1/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lista_de_Exercicios_1
+{
+    public partial class Form2 : Form
+    {
+        int calculaMedia, estoqueMinimo, estoqueMaximo;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBox_Leave2(object sender, EventArgs e)
+        {
+            estoqueMaximo = int.Parse(textBox2.Text);
+        }
+
+
+        private void txtBox_Leave1(object sender, EventArgs e)
+        {
+            estoqueMinimo = int.Parse(textBox1.Text);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"O Estoque médio é {(estoqueMinimo + estoqueMaximo) / 2}");
+        }
+    }
+}
